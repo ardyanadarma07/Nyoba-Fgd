a53b6949e83a062d2f3b11c9a33605b0af51a7db
Ardyana Darma <ardyanadarma@gmail.com>
2024-11-08 21:52:06 +0800

    menambahkan fungsi

--- khusus_fgd.c
+++ khusus_fgd.c
@@ -4,4 +4,11 @@
 void main (){
     system("cls");
     printf ("Hello World");
+    int a, b, total;
+    printf ("masukkan angka 1 : ");
+    scanf("%d", &a);
+    printf("Masukkan angka 2 : ");
+    scanf("%d", &b);
+    total = a + b;
+    printf("Total : %d", total);
 }
